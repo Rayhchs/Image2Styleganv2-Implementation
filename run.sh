0fb7e2a89fda363dfe4c0ea675cd5d52807ef68f
Rayhchs <ray1515123@gmail.com>
2023-09-18 09:12:58 +0800

    run

---
+++ run.sh
@@ -0,0 +1,4 @@
+python main.py --img_a 'obama.jpg' --phase 'reconstruction'
+python main.py --img_a 'obama.jpg' --img_b 'Ryan.jpg' --phase 'crossover'
+python main.py --img_a 'obama.jpg' --phase 'inpainting'
+python main.py --img_a 'obama.jpg' --img_b 'sketch.png' --phase 'style_transfer'
